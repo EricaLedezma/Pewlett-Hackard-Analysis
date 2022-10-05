f2eac66a109fe996e44f661fe49b1254a37d87de
Ledezma <eledezm@amazon.com>
2022-10-04 17:30:05 -0700

    added analysis query

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -48,3 +48,21 @@ on (e.emp_no = ti.emp_no)
 WHERE de.end_date = ('9999-01-01')
 AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
 ORDER BY emp_no;
+
+-- MENTORSHIP ELIGIBILITY - BROADENED to 2 YEARS 64-65
+SELECT DISTINCT ON (emp_no) e.emp_no,
+e.first_name,
+e.last_name,
+e.birth_date,
+de.from_date,
+de.end_date,
+ti.title
+-- INTO mentorship_eligibility
+FROM employees as e
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles as ti
+on (e.emp_no = ti.emp_no)
+WHERE de.end_date = ('9999-01-01')
+AND (e.birth_date BETWEEN '1964-01-01' AND '1965-12-31')
+ORDER BY emp_no;
